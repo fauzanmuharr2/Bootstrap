eb871e9540fd9398f3e65c0c9b6a609071cfa715
Fauzanmuharr2 <fauzanmuharr21@gmail.com>
2019-09-25 08:35:50 +0700

    Bootstrap

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Latihan Bootstrap</title>
+    <!-- Pemanggilan CSS -->
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <!-- Akhir Pemanggilan CSS -->
+</head>
+
+<body>
+         <!-- Header -->
+         <!-- End Header -->
+
+         <!-- Content -->
+         <div class="container">  
+              <div class="row">
+                 <div class="col-md-12" style="background:violet">
+                   <h1><center>12 Column</center></h1>
+                 </div>
+              </div>
+                    <div class="row">
+                        <div class="col-md-6" style="background:red">
+                          <h1>6 Column</h1>
+                        </div>
+                            <div class="col-md-6" style="background:blue">
+                           <h1>6 Column</h1>
+                        </div>
+                    </div> 
+         </div>
+         <!-- End Content -->
+
+         <!-- Footer -->
+         <!-- End Footer -->
+
+         <!-- Pemanggilan JS -->
+         <script src="/asset/js/bootstrap.bundle.js"></script>
+         <script src="/asset/js/bootstrap.bundle.min.js"></script>
+         <!-- Akhir Pemanggilan JS -->
+</body>
+</html>
